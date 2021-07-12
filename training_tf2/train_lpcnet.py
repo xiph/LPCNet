e83af6c191a1747f0b1152c9c3f379cbcb3b530c
Jean-Marc Valin <jmvalin@amazon.com>
2021-07-12 14:48:34 -0400

    Oops, fix quantization

--- training_tf2/train_lpcnet.py
+++ training_tf2/train_lpcnet.py
@@ -96,7 +96,7 @@ opt = Adam(lr, decay=decay, beta_2=0.99)
 strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 
 with strategy.scope():
-    model, _, _ = lpcnet.new_lpcnet_model(rnn_units1=args.grua_size, training=True)
+    model, _, _ = lpcnet.new_lpcnet_model(rnn_units1=args.grua_size, training=True, quantize=quantize)
     model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
     model.summary()
 
