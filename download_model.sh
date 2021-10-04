88f390e22c6848f57b7f941201220f482d1441de
Jean-Marc Valin <jmvalin@amazon.com>
2021-10-04 17:02:29 -0400

    Adding downloads for P192/P384/P640 models
    P384 is the same as curent master

--- download_model.sh
+++ download_model.sh
@@ -4,7 +4,7 @@ model=lpcnet_data-$1.tar.gz
 
 if [ ! -f $model ]; then
         echo "Downloading latest model"
-        wget https://media.xiph.org/lpcnet/data/$model
+        wget https://media.xiph.org/lpcnet/data/lpcnet_efficiency/$model
 fi
 tar xvf $model
-
+touch src/nnet_data.[ch]
